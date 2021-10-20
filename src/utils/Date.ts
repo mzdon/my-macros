1fc436d85c2485d1d2e5ee0c9485237ed34ae98f
Marc Zdon <marczdon@gmail.com>
2021-10-20 13:22:32 -0400

    Don't use UTC for today()

--- src/utils/Date.ts
+++ src/utils/Date.ts
@@ -1,18 +1,6 @@
 export const today = () => {
   const d = new Date();
-  return new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-};
-
-export const getDateWithoutTime = (date: Date | string = new Date()) => {
-  const d = date instanceof Date ? date : new Date(date);
-  return new Date(
-    d.getUTCFullYear(),
-    d.getUTCMonth(),
-    d.getUTCDate(),
-    (d.getTimezoneOffset() / 60) * -1,
-    0,
-    0,
-  );
+  return new Date(d.getFullYear(), d.getMonth(), d.getDate());
 };
 
 export const isSameDay = (a: Date, b: Date) => {
